15212d4dc3dd81949cd35cc15ea37dfba90e95e8
Igor Kovalev <igor.alex.kovalev@gmail,com>
2023-04-10 08:40:07 +0300

    Initialize first commit.

---
+++ src/main/java/ru/techmeskills/Main.java
@@ -0,0 +1,4 @@
+package ru.techmeskills;
+
+public class Main {
+}
--- src/main/java/ru/techmeskills/homework1/Test.java
+++ src/main/java/ru/techmeskills/homework1/Test.java
@@ -1,3 +1,5 @@
+package ru.techmeskills.homework1;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
